9c2d48373d11cb2f23834606727a8879c87fc532
Robert Rabe <robert@rrabe.com>
2020-03-20 23:40:41 +0100

    Setup project

---
+++ backend/src/main/java/tuersteher/Application.java
@@ -0,0 +1,14 @@
+package tuersteher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Robert Rabe on 20.03.20.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
