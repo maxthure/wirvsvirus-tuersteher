d0775e40c2380148055f1f93591beb0f6fe583b6
Thure <th.nebendahl@gmail.com>
2020-03-21 09:28:23 +0100

    Added first Controller and Template
    Added a first controller and a first template to establish project structure. 
    Index Controller is useless atm as Spring Boot uses the template "index.html" for the url "/" anyway.

---
+++ src/main/java/tuersteher/controller/IndexController.java
@@ -0,0 +1,16 @@
+package tuersteher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Thure Nebendahl on 21.03.20
+ */
+@Controller
+public class IndexController {
+/*
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }*/
+}
