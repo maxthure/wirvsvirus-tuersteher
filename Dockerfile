218699a92e0077d0bda05ced95f1ca26dc716821
Robert Rabe <robert@rrabe.com>
2020-03-21 10:09:49 +0100

    Update test dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
-FROM openjdk:11-jdk-slim
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+# Maven build flow adapted from https://stackoverflow.com/questions/27767264/how-to-dockerize-maven-project-and-how-many-ways-to-accomplish-it
+#
+# Build stage
+#
+FROM maven:3.6.0-jdk-11-slim AS build
+COPY src /home/src
+COPY pom.xml /home/
+RUN mvn -f /home/pom.xml clean package
+
+#
+# Package stage
+#
+FROM openjdk:11-jre-slim
+COPY --from=build /home/target/*.jar /usr/local/lib/app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-jar","/usr/local/lib/app.jar"]
--- src/main/java/tuersteher/Application.java
+++ src/main/java/tuersteher/Application.java
@@ -3,12 +3,20 @@ package tuersteher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 /**
  * @author Robert Rabe on 20.03.20.
  */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
+        app.setDefaultProperties(Collections.singletonMap("server.port", port));
         SpringApplication.run(Application.class, args);
     }
 }
