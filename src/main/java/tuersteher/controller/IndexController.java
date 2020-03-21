5c3e433d3e0ab1ea7ed0792e24659398aec14058
Thure <th.nebendahl@gmail.com>
2020-03-21 14:05:19 +0100

    Added a quick visual

--- src/main/java/tuersteher/controller/IndexController.java
+++ src/main/java/tuersteher/controller/IndexController.java
@@ -1,7 +1,11 @@
 package tuersteher.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * @author Thure Nebendahl on 21.03.20
@@ -11,5 +15,20 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/questionnaire")
+    String register(Model model, QuestionnaireForm form) {
+        model.addAttribute("form", form);
+        return "questionnaire";
+    }
+
+    @PostMapping("/questionnaire")
+    String registerNew(@Valid @ModelAttribute("form") QuestionnaireForm form, Errors result, Model model) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/tuersteher/controller/QuestionnaireForm.java
@@ -0,0 +1,75 @@
+package tuersteher.controller;
+
+/**
+ * @author Thure Nebendahl on 21.03.20
+ */
+public class QuestionnaireForm {
+    String firstname;
+    String lastname;
+    String email;
+    String street;
+    String postcode;
+    String city;
+    String country;
+
+    public QuestionnaireForm(){
+
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return "";
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return "";
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return "";
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return "";
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPostcode() {
+        return "";
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return "";
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return "";
+    }
+
+}
